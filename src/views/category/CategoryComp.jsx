c667ae2f98bf9ca696a7e5919543a7ed0f357866
Krishrpatel <krishrpatel250@gmail.com>
2025-12-11 17:55:15 +0530

    notify : coming soon

--- src/views/category/CategoryComp.jsx
+++ src/views/category/CategoryComp.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from "react";
 
 const CategoryComp = () => {
   return (
-    <div>CategoryComp</div>
-  )
-}
+    <div className="flex items-center justify-center h-[80vh]">
+      <h1 className="text-3xl font-bold text-gray-600"> Coming Soon...</h1>
+    </div>
+  );
+};
 
-export default CategoryComp
+export default CategoryComp;
