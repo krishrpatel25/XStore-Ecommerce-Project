9ad8c5e1651dfc4efe89494156129c5c1e23d6f1
Krishrpatel <krishrpatel250@gmail.com>
2025-12-16 19:59:39 +0530

    make :ordre page

---
+++ src/pages/order/OrderPage.jsx
@@ -0,0 +1,10 @@
+import OrderPageComp from '@/views/order/OrderPageComp'
+import React from 'react'
+
+const OrderPage = () => {
+  return (
+    <OrderPageComp/>
+  )
+}
+
+export default OrderPage
