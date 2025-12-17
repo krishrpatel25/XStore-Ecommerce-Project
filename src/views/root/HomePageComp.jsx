ba56c32903b26aa4cedec8bee8fe2b35a86f132d
Krishrpatel <krishrpatel250@gmail.com>
2025-12-17 15:57:27 +0530

    remove unwanted imports

--- src/views/root/HomePageComp.jsx
+++ src/views/root/HomePageComp.jsx
@@ -1,6 +1,5 @@
-import React, { useEffect, useState } from "react";
+import React, {  useState } from "react";
 import axios from "axios";
-import { toast } from "react-hot-toast";
 import { useNavigate } from "react-router-dom";
 import Autoplay from "embla-carousel-autoplay";
 import {
