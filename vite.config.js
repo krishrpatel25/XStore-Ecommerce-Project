52f9962e849d110431f24bb34dc72d3586ef7d99
Krishrpatel <krishrpatel250@gmail.com>
2025-12-19 18:30:43 +0530

    add : scripts for deploy

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,20 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import tailwindcss from '@tailwindcss/vite';
-import path from 'path';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import path from "path";
 
 export default defineConfig({
+  base: "/XStore-Ecommerce-Project/", // ⭐ REQUIRED for GitHub Pages
   plugins: [
     tailwindcss(),
     react(),
   ],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, 'src'),
+      "@": path.resolve(__dirname, "src"),
     },
   },
   build: {
-    sourcemap: false, // ✅ hides original source in production
+    sourcemap: false,
   },
 });
